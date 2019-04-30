67961cb99c03760aac064b3b29ebcf7ae6f424a7
richardevcom <17192512+richardevcom@users.noreply.github.com>
2019-05-01 02:42:19 +0300

    Fix - Call to a member function hasRole() on null
    Fix for "Call to a member function hasRole() on null". If the user is not logged in, there is no user data to process, so we need to throw 404 code.

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -18,12 +18,21 @@ class RoleMiddleware
 	 */
     public function handle($request, Closure $next, $role, $permission = null)
     {
+	/**
+	 * Fix for "Call to a member function hasRole() on null".
+	 * If the user is not logged in, there is no user data to process,
+	 * so we need to throw 404 code.
+	 */
+	if(is_null($request->user())){
+            abort(404);
+        }
+	    
     	if(!$request->user()->hasRole($role)) {
 			abort(404);
 	    }
-	    if($permission !== null && !$request->user()->can($permission)) {
-    		abort(404);
-	    }
+	if($permission !== null && !$request->user()->can($permission)) {
+		abort(404);
+	}
         return $next($request);
     }
 }
