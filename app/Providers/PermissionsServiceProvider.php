02b7de3f28448ceecece7c9ad45477e312ee193a
usamamuneerchaudhary <info@usamamuneer.me>
2019-10-08 22:55:00 +0500

    permission service provider fix

--- app/Providers/PermissionsServiceProvider.php
+++ app/Providers/PermissionsServiceProvider.php
@@ -16,19 +16,25 @@ class PermissionsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Permission::get()->map(function($permission){
-			Gate::define($permission->slug, function($user) use ($permission){
-				return $user->hasPermissionTo($permission);
-			});
-        });
+        try {
+            Permission::get()->map(function ($permission) {
+                Gate::define($permission->slug, function ($user) use ($permission) {
+                    return $user->hasPermissionTo($permission);
+                });
+            });
+        } catch (\Exception $e) {
+            report($e);
+            return false;
+        }
+
 
         //Blade directives
-	    Blade::directive('role', function ($role){
-			return "<?php if(auth()->check() && auth()->user()->hasRole({$role})) : ?>";
-	    });
-	    Blade::directive('endrole', function ($role){
-		    return "<?php endif; ?>";
-	    });
+        Blade::directive('role', function ($role) {
+            return "<?php if(auth()->check() && auth()->user()->hasRole({$role})) : ?>";
+        });
+        Blade::directive('endrole', function ($role) {
+            return "<?php endif; ?>";
+        });
     }
 
     /**
@@ -40,4 +46,5 @@ class PermissionsServiceProvider extends ServiceProvider
     {
         //
     }
+
 }
