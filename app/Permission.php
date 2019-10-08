c3b17c756ec61958a3800040620fe83fdbfa66a6
usamamuneerchaudhary <info@usamamuneer.me>
2019-10-08 23:29:46 +0500

    roles and permissions updated

--- app/Permission.php
+++ app/Permission.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
 	public function roles() {
 		return $this->belongsToMany(Role::class,'roles_permissions');
     }
--- app/Role.php
+++ app/Role.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function permissions() {
     	return $this->belongsToMany(Permission::class,'roles_permissions');
     }
